e8ef4b74fc6d53e7de5b5692a18bf2de6af009f5
blindbeast <v_zemlyannikov@mail.ru>
2025-10-26 21:00:43 +0300

    edit users/view.py

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.decorators import
 from django.views import View
 from .forms import SignUpForm, UserUpdateForm
 
