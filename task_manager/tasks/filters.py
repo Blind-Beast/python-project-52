b1a60a3adc2ea4c8ab25a09f1e9705b5a88bdaad
blindbeast <v_zemlyannikov@mail.ru>
2025-11-28 22:20:01 +0300

    edit tasks/filters.py

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -9,22 +9,26 @@ class TaskFilter(django_filters.FilterSet):
     status = django_filters.ModelChoiceFilter(
         queryset=Status.objects.all(),
         label="Статус",
+        label_suffix='',
         widget=forms.Select(attrs={'class': 'form-select'})
     )
     executor = django_filters.ModelChoiceFilter(
         queryset=CustomUser.objects.all(),
         label="Исполнитель",
+        label_suffix='',
         widget=forms.Select(attrs={'class': 'form-select'})
     )
     labels = django_filters.ModelChoiceFilter(
         queryset=Label.objects.all(),
         label="Метка",
+        label_suffix='',
         widget=forms.Select(attrs={'class': 'form-select'})
     )
     self_tasks = django_filters.BooleanFilter(
         field_name="author",
         method="show_self_tasks",
         label="Только свои задачи",
+        label_suffix='',
         widget=forms.CheckboxInput(attrs={'class': 'form-check-input',})
     )
 
