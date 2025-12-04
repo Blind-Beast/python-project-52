d651b61361991345e4154a3028468c96ad21a598
blindbeast <v_zemlyannikov@mail.ru>
2025-12-04 21:50:47 +0300

    fix lint errors

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
 from .models import CustomUser
 
